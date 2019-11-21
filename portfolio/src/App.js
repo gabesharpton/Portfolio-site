0947d1e68c0a6650571ca9e604fb029476e1b8fd
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-21 16:44:01 -0600

    fixed up intro paragraph

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <a href="https://gabesharpton.github.io/Hangman-Javascript-Game/">My Hangman Javascript Browser Game</a>
-        <a href="https://fitness-planner-gls.herokuapp.com/">My first Back-end App</a>
+        
+        <a href="https://gabesharpton.github.io/Hangman-Javascript-Game/">Hangman Game</a>
+        <a href="https://fitness-planner-gls.herokuapp.com/">Back-end App</a>
         <a href="https://github.com/wtsia/DevQ">DevQ</a>
         
         
