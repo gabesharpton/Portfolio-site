1a9f4036ccaee65172d6c0c1453b0ee3911401bc
gabesharpton <gabriel.sharpton@gmail.com>
2019-11-23 18:55:46 -0600

    trying react popup

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hi')
+export default Modal = () => (
+    <Popup
+      trigger={<button className="button"> Open Modal </button>}
+      modal
+      closeOnDocumentClick
+    >
+      <span> Modal content </span>
+    </Popup>
+  );
+  
+  render(<Modal />);
